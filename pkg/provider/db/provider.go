1225c10aa6d62b56ba7a39ba1257505933241e7a
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2020-04-24 21:49:00 +0200

    refactor: create only one db access struct and share it

--- pkg/provider/db/provider.go
+++ pkg/provider/db/provider.go
@@ -9,17 +9,18 @@ import (
 
 type ImageProvider struct {
 	dataSourceName string
+	db             *sql.DB
 }
 
-func (i ImageProvider) Images() ([]pkg.Image, error) {
-	db, err := sql.Open("postgres", i.dataSourceName)
-	if err != nil {
-		return nil, err
+func (i *ImageProvider) Images() ([]pkg.Image, error) {
+	if i.db == nil {
+		if err := i.connect(); err != nil {
+			fmt.Printf("could not connect to db %q", err)
+			return nil, err
+		}
 	}
 
-	defer db.Close()
-
-	rows, err := db.Query("SELECT id, name, image as path FROM locations_photo WHERE visibility = 3 ORDER BY id DESC")
+	rows, err := i.db.Query("SELECT id, name, image as path FROM locations_photo WHERE visibility = 3 ORDER BY id DESC")
 	if err != nil {
 		return nil, err
 	}
@@ -39,31 +40,41 @@ func (i ImageProvider) Images() ([]pkg.Image, error) {
 	return images, nil
 }
 
-func (i ImageProvider) Get(id string) *pkg.Image {
-	db, err := sql.Open("postgres", i.dataSourceName)
-	if err != nil {
-		return nil
+func (i *ImageProvider) Get(id string) *pkg.Image {
+	if i.db == nil {
+		if err := i.connect(); err != nil {
+			fmt.Printf("could not connect to db %q", err)
+			return nil
+		}
 	}
 
-	defer db.Close()
-
 	var image pkg.Image
-	row := db.QueryRow("SELECT id, name, image as path FROM locations_photo WHERE visibility = 3 AND id = $1", id)
-	err = row.Scan(&image.Id, &image.Name, &image.Path)
+	row := i.db.QueryRow("SELECT id, name, image as path FROM locations_photo WHERE visibility = 3 AND id = $1", id)
+	err := row.Scan(&image.Id, &image.Name, &image.Path)
 	if err != nil {
 		return nil
 	}
 	return &image
 }
 
-func New(dataSourceString string) ImageProvider {
-	return ImageProvider{
+func (i *ImageProvider) connect() error {
+	db, err := sql.Open("postgres", i.dataSourceName)
+	if err != nil {
+		return err
+	}
+	i.db = db
+
+	return nil
+}
+
+func New(dataSourceString string) *ImageProvider {
+	return &ImageProvider{
 		dataSourceName: dataSourceString,
 	}
 }
 
-func NewFromCredentials(host string, username string, password string, port uint, database string) ImageProvider {
-	return ImageProvider{
+func NewFromCredentials(host string, username string, password string, port uint, database string) *ImageProvider {
+	return &ImageProvider{
 		dataSourceName: fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", username, password, host, port, database),
 	}
 }
