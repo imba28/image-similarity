50aa8fe60ebd8a2381f67eace8dc50da6ef21f5d
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2020-04-26 21:06:22 +0200

    test: add file image provider Get() test

--- pkg/provider/file/provider_test.go
+++ pkg/provider/file/provider_test.go
@@ -1,6 +1,35 @@
 package file
 
-import "testing"
+import (
+	"imba28/images/pkg"
+	"testing"
+)
+
+func TestImageProvider_Get(t *testing.T) {
+	provider := New("../../../template")
+	image := provider.Get("index.html")
+
+	if image == nil {
+		t.Errorf("should have returned image, got: %v", nil)
+		return
+	}
+
+	if image.Name != "index.html" {
+		t.Errorf("incorrect image name, got: %q, want: %q", image.Name, "index.html")
+	}
+	if image.Path != "../../../template/index.html" {
+		t.Errorf("incorrect image path, got: %q, want: %q", image.Path, "../../../template/index.html")
+	}
+	if image.Guid != 0 {
+		t.Errorf("incorrect image guid, got: %d, want: %d", image.Guid, 0)
+	}
+	if image.Features != nil {
+		t.Errorf("incorrect image feature vector, got: %v, want: %v", image.Features, nil)
+	}
+	if image.Id != "index.html" {
+		t.Errorf("incorrect image name, got: %q, want: %q", image.Name, "index.html")
+	}
+}
 
 func TestImageProvider_Images(t *testing.T) {
 	provider := New("../../../template")
@@ -11,6 +40,14 @@ func TestImageProvider_Images(t *testing.T) {
 	}
 	if len(files) != 2 {
 		t.Errorf("Length of files incorrect, got: %d, want: %d", len(files), 2)
+	}
+}
+
+func TestImageProvider_Images_not_existing(t *testing.T) {
+	provider := New("../foobar")
+	_, err := provider.Images()
+	if err == nil {
+		t.Errorf("if directory does not exist Images() should return an error")
 	}
 }
 
@@ -55,5 +92,38 @@ func TestImageProvider_Images_hidden_files(t *testing.T) {
 	}
 	if len(files) != 5 {
 		t.Errorf("Length of files non hidden files incorrect, got: %d, want: %d", len(files), 5)
+	}
+}
+
+func TestImageProvider_Persist(t *testing.T) {
+	provider := New("../../../") // project root dir
+	image := pkg.Image{
+		Id:       "2",
+		Guid:     2,
+		Path:     "foo/bar.png",
+		Name:     "",
+		Features: []float64{1, 2, 3},
+	}
+	if err := provider.Persist(&image); err != nil {
+		t.Errorf("file provider should never return an error as this is a noop operation, got: %q", err)
+	}
+}
+
+func TestNewImage(t *testing.T) {
+	image := NewImage("/locations/foo/hello-world.png")
+	if image.Name != "hello-world.png" {
+		t.Errorf("incorrect image name, got: %q, want: %q", image.Name, "hello-world.png")
+	}
+	if image.Path != "/locations/foo/hello-world.png" {
+		t.Errorf("incorrect image path, got: %q, want: %q", image.Path, "/locations/foo/hello-world.png")
+	}
+	if image.Guid != 0 {
+		t.Errorf("incorrect image guid, got: %d, want: %d", image.Guid, 0)
+	}
+	if image.Features != nil {
+		t.Errorf("incorrect image feature vector, got: %v, want: %v", image.Features, nil)
+	}
+	if image.Id != "hello-world.png" {
+		t.Errorf("incorrect image name, got: %q, want: %q", image.Name, "hello-world.png")
 	}
 }
