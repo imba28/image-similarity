bd663c6844e5c12c05391b99d2fca3434abf59fe
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2020-04-26 14:40:21 +0200

    test: test if file provider returns hidden files

--- pkg/provider/file/provider_test.go
+++ pkg/provider/file/provider_test.go
@@ -13,3 +13,15 @@ func TestImageProvider_Images(t *testing.T) {
 		t.Errorf("Length of files incorrect, got: %d, want: %d", len(files), 2)
 	}
 }
+
+func TestImageProvider_Images_hidden_files(t *testing.T) {
+	provider := New("../../../") // project root dir
+
+	files, err := provider.Images()
+	if err != nil {
+		t.Error("ImageProvider should not return error")
+	}
+	if len(files) != 5 {
+		t.Errorf("Length of files non hidden files incorrect, got: %d, want: %d", len(files), 5)
+	}
+}
